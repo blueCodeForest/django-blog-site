98c139af0f6130f4c42017f3da2a22633b69c110
blueCodeForest <tokinari.pp@gmail.com>
2021-06-07 11:51:09 +0900

    fix allowed_hosts

--- dbsite/settings/production.py
+++ dbsite/settings/production.py
@@ -6,7 +6,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['django-blog-site-5616.herokuapp.com']
+# ALLOWED_HOSTS = ['django-blog-site-5616.herokuapp.com']
+ALLOWED_HOSTS = ['*']
 
 import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=600)
