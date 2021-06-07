2efbe86a141fbf34871cb1e1d4984f67dbf086a0
blueCodeForest <tokinari.pp@gmail.com>
2021-06-07 12:22:26 +0900

    tweak

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn dbsite.wsgi --log-file -
+web: gunicorn config.wsgi --log-file -
--- dbsite/settings/production.py
+++ dbsite/settings/production.py
@@ -6,8 +6,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-# ALLOWED_HOSTS = ['django-blog-site-5616.herokuapp.com']
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['django-blog-site-5616.herokuapp.com']
 
 import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=600)
