b42a6bbcdb2c72734349237606f9dc9e74845961
blueCodeForest <tokinari.pp@gmail.com>
2021-06-07 16:08:36 +0900

    twaek

--- dbsite/settings/base.py
+++ dbsite/settings/base.py
@@ -119,3 +119,6 @@ STATICFILES_DIRS = [
 ]
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+db_from_env = dj_database_url.config(conn_max_age=600, ssl_require=True)
+DATABASES['default'].update(db_from_env)
